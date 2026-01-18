49aba0bd4a75ceb7ad2bc29bb8e38bc110a92258
FR Lyvers <verslyfr@github.com>
2026-01-18 06:36:45 -0500

    Update config: Set WSL default, add Launch Menu, change font to Hack Nerd Font

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,21 @@ local config = wezterm.config_builder()
 
 -- Basic configuration
 config.color_scheme = 'Batman'
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'Hack Nerd Font'
+
+-- Default to WSL
+config.default_prog = { 'wsl.exe' }
+
+-- Launch Menu
+config.launch_menu = {
+  {
+    label = 'Git Bash',
+    args = { 'C:\\Users\\versl\\scoop\\apps\\git\\current\\bin\\bash.exe', '-l' },
+  },
+  {
+    label = 'PowerShell 7',
+    args = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' },
+  },
+}
 
 return config

80445b621036080987d6b04e588488b9deb65e80
FR Lyvers <verslyfr@github.com>
2026-01-23 04:31:38 -0500

    Changed the default theme to Alabaster.

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- ============================================================
 -- Basic configuration
-config.color_scheme = 'Batman'
+-- ============================================================
+config.color_scheme = 'Alabaster'
 config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 10.0
 
@@ -16,28 +18,34 @@ config.launch_menu = {
     args = { 'C:\\Users\\versl\\scoop\\apps\\git\\current\\bin\\bash.exe', '-l' },
   },
   {
-        label = 'PowerShell 7',
-        args = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' },
-      },
-    }
-    
-    -- Keybindings
-    config.keys = {
-      {
-        key = '|',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-      },
-      {
-            key = '_', -- 'underscore' corresponds to 'Shift + -'
-            mods = 'CTRL|SHIFT',
-            action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-          },
-          {
-            key = '0',
-            mods = 'CTRL|SHIFT',
-            action = wezterm.action.CloseCurrentPane { confirm = true },
-          },
-        }
-            return config
-    
+    label = 'PowerShell 7',
+    args = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' },
+  },
+}
+
+wezterm.log_info("Configuration reloaded.")
+
+-- ============================================================
+-- Keybindings
+-- ============================================================
+config.keys = {
+  {
+    key = '|',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '_', -- Shift + -
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '0',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+-- ============================================================
+-- Final return
+-- ============================================================
+return config
